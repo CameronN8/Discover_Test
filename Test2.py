69c95a76b74240024e1540fae1b4fcdbc42892bf
CameronN8 <145394371+CameronN8@users.noreply.github.com>
2024-06-25 22:49:13 -0400

    Update Test2.py

--- Test2.py
+++ Test2.py
@@ -1,6 +1,16 @@
-import time
-print("T2 ping")
-time.sleep(0.5)
-print("T2 ping 2")
-time.sleep(1)
-print("Test 2 also complete")
+#!/usr/bin/env python3
+
+from rover_api.discover_rover import Rover
+from rover_api.discover_camera import Camera
+from rover_api.discover_utils import finish_experiment
+
+rover = Rover()
+cam = Camera()
+
+
+for i in range(4):
+    rover.move_forward(0.2, 3)
+    cam.get_jpg()
+    rover.turn_right(15, 6) 
+
+finish_experiment()
